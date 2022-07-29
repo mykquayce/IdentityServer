c0f5f8e35bc9f18d4f64f292eeeba224d99ab4c7
bob h <mail@bob.codes>
2022-07-29 01:03:32 +0100

    refresh script

---
+++ refresh.ps1
@@ -0,0 +1,16 @@
+﻿docker pull mcr.microsoft.com/dotnet/aspnet:7.0
+docker pull mcr.microsoft.com/dotnet/sdk:7.0
+docker pull eassbhhtgu/identity-server:latest
+
+$base1 = docker image inspect --format '{{.Created}}' mcr.microsoft.com/dotnet/aspnet:7.0
+$base2 = docker image inspect --format '{{.Created}}' mcr.microsoft.com/dotnet/sdk:7.0
+$target = docker image inspect --format '{{.Created}}' eassbhhtgu/identity-server:latest
+
+if ($base1 -gt $target -or $base2 -gt $target) {
+
+	docker build --tag eassbhhtgu/identity-server:latest .
+	if (!$?) { exit 1; }
+
+	docker push eassbhhtgu/identity-server:latest
+	if (!$?) { exit 1; }
+}
