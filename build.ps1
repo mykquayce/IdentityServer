1ecc1b5309cd6b2e42933f2c106c3e671955bdd5
bob h <mail@bob.codes>
2024-10-27 20:45:20 +0000

    tidying / updates

--- build.ps1
+++ build.ps1
@@ -1,8 +1,11 @@
-docker pull mcr.microsoft.com/dotnet/sdk:8.0
+docker pull mcr.microsoft.com/dotnet/sdk:8.0-alpine
 if (!$?) { return; }
-docker pull mcr.microsoft.com/dotnet/aspnet:8.0
+docker pull mcr.microsoft.com/dotnet/aspnet:8.0-alpine
 if (!$?) { return; }
 docker build `
+	--file '.\IdentityServer.WebApplication\Dockerfile' `
 	--secret "id=ca_crt,src=${env:userprofile}\.aspnet\https\ca.crt" `
 	--tag eassbhhtgu/identity-server:latest `
 	.
+if (!$?) { return; }
+docker push eassbhhtgu/identity-server:latest
--- deploy.ps1
+++
@@ -1,3 +0,0 @@
-docker pull eassbhhtgu/identity-server:latest
-if (!$?) { return; }
-docker stack deploy --compose-file .\docker-compose.yml identity-server
